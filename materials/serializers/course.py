e96c9009a9406c1ab20da9444b91165b0da51a53
alexander <vertiporogov.alexandr@mail.ru>
2024-03-09 16:21:11 +0300

    homework 2 finish first task

--- materials/serializers/course.py
+++ materials/serializers/course.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
+from rest_framework.fields import SerializerMethodField
 
-from materials.models import Course
+from materials.models import Course, Lesson
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    count_lesson = SerializerMethodField()
+
+    def get_count_lesson(self, course):
+        return Lesson.objects.filter(course=course).count()
+
     class Meta:
         model = Course
-        fields = '__all__'
+        fields = ('name', 'description', 'count_lesson',)
